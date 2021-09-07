051b048b3d21f43506d70f9a6c65d7d3861d8a88
SaxonJude <saxonjude@gmail.com>
2021-09-07 12:52:19 +0100

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import ReactDOM from 'react-dom';
+import './styles/style.scss';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Index</h1>
+        </div>
+    )
+}
+
+export default App;
---
+++ client/src/components/qwe.js
@@ -0,0 +1,12 @@
+import React from 'React';
+import style from '../styles/style.scss';
+
+const header = () => {
+    return (
+        <div className="header">
+            
+        </div>
+    )
+}
+
+export default header;
